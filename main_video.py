8b679cffccb63aee250fe888a80f8f62b72c497e
telsamar <56135473+telsamar@users.noreply.github.com>
2023-08-20 23:17:45 +0300

    ss

--- main_video.py
+++ main_video.py
@@ -12,67 +12,9 @@ from ultralytics import YOLO
 from blockchain import write_block
 from deepface import DeepFace
 
-"""Захват видеопотока с камеры."""
-def get_camera_capture(camera_num: int = 0) -> cv2.VideoCapture:
-    cap = cv2.VideoCapture(camera_num)
-    return cap
-
-"""Захват видеопотока из файла."""
-def get_file_capture(video_path: str) -> cv2.VideoCapture:
-    if not os.path.exists(video_path):
-        raise FileNotFoundError(f"Файл {video_path} не найден.")
-    cap = cv2.VideoCapture(video_path)
-    return cap
-
-"""Добавляет рамку и текст к обрезанному изображению."""
-def draw_frame_on_cropped_img(cropped_img: cv2.imread, text: str, ph_conf: float, thickness: int = 2, color: tuple = (0, 255, 0)) -> cv2.imread:
-    framed_cropped_img = cropped_img.copy()
-    h, w, _ = cropped_img.shape
-    cv2.rectangle(framed_cropped_img, (0, 0), (w, h), color, thickness)
-
-    # Настройка параметров шрифта
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    font_color = (0, 0, 255)
-    font_thickness = 2
-    max_text_width = int(w * 0.9)
-    font_scale = 1
-    text_width = w
-    while text_width > max_text_width:
-        font_scale -= 0.1
-        (text_width, text_height), _ = cv2.getTextSize(text, font, font_scale, font_thickness)
-
-    text_x = (w - text_width) // 2
-    text_y = text_height + 10 
-
-    cv2.putText(framed_cropped_img, text, (text_x, text_y), font, font_scale, font_color, font_thickness)
-
-    # Добавление дополнительной строки текста
-    second_line_text = str(round(ph_conf, 2))
-    second_line_text_width = cv2.getTextSize(second_line_text, font, font_scale, font_thickness)[0][0]
-    second_line_text_x = (w - second_line_text_width) // 2
-    second_line_text_y = text_y + text_height + 10
-    cv2.putText(framed_cropped_img, second_line_text, (second_line_text_x, second_line_text_y), font, font_scale, font_color, font_thickness)
-
-    return framed_cropped_img
-
-"""Возвращает путь к последнему созданному файлу в указанной директории."""
-def get_last_created_file_in_images_full_photo(directory):
-    files = os.listdir(directory)
-    paths = [os.path.join(directory, file) for file in files]
-    paths.sort(key=os.path.getmtime)
-    return paths[-1] if paths else None
-
-"""Очищает содержимое указанной директории."""
-def clear_directory(directory: str):
-    for file in os.listdir(directory):
-        file_path = os.path.join(directory, file)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-
 def main():
-    video_path = "videos/1.mp4"
-
-    try:
+    try:    
+        # video_path = "videos/1.mp4"
         # cap = get_file_capture(video_path)
         cap = get_camera_capture()
 
@@ -161,6 +103,63 @@ def main():
         print(f"Ошибка: {e}")
     except Exception as e:
         print(f"Произошла непредвиденная ошибка: {e}")
+
+"""Захват видеопотока с камеры."""
+def get_camera_capture(camera_num: int = 0) -> cv2.VideoCapture:
+    cap = cv2.VideoCapture(camera_num)
+    return cap
+
+"""Захват видеопотока из файла."""
+def get_file_capture(video_path: str) -> cv2.VideoCapture:
+    if not os.path.exists(video_path):
+        raise FileNotFoundError(f"Файл {video_path} не найден.")
+    cap = cv2.VideoCapture(video_path)
+    return cap
+
+"""Добавляет рамку и текст к обрезанному изображению."""
+def draw_frame_on_cropped_img(cropped_img: cv2.imread, text: str, ph_conf: float, thickness: int = 2, color: tuple = (0, 255, 0)) -> cv2.imread:
+    framed_cropped_img = cropped_img.copy()
+    h, w, _ = cropped_img.shape
+    cv2.rectangle(framed_cropped_img, (0, 0), (w, h), color, thickness)
+
+    # Настройка параметров шрифта
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_color = (0, 0, 255)
+    font_thickness = 2
+    max_text_width = int(w * 0.9)
+    font_scale = 1
+    text_width = w
+    while text_width > max_text_width:
+        font_scale -= 0.1
+        (text_width, text_height), _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+
+    text_x = (w - text_width) // 2
+    text_y = text_height + 10 
+
+    cv2.putText(framed_cropped_img, text, (text_x, text_y), font, font_scale, font_color, font_thickness)
+
+    # Добавление дополнительной строки текста
+    second_line_text = str(round(ph_conf, 2))
+    second_line_text_width = cv2.getTextSize(second_line_text, font, font_scale, font_thickness)[0][0]
+    second_line_text_x = (w - second_line_text_width) // 2
+    second_line_text_y = text_y + text_height + 10
+    cv2.putText(framed_cropped_img, second_line_text, (second_line_text_x, second_line_text_y), font, font_scale, font_color, font_thickness)
+
+    return framed_cropped_img
+
+"""Возвращает путь к последнему созданному файлу в указанной директории."""
+def get_last_created_file_in_images_full_photo(directory):
+    files = os.listdir(directory)
+    paths = [os.path.join(directory, file) for file in files]
+    paths.sort(key=os.path.getmtime)
+    return paths[-1] if paths else None
+
+"""Очищает содержимое указанной директории."""
+def clear_directory(directory: str):
+    for file in os.listdir(directory):
+        file_path = os.path.join(directory, file)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
 if __name__ == '__main__':
     main()
