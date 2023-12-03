66eaf3594988a7e4ebd246ba4b1df0040fc6b9c0
telsamar <56135473+telsamar@users.noreply.github.com>
2023-12-03 13:13:20 +0300

    a

--- main_video.py
+++ main_video.py
@@ -9,20 +9,18 @@ import re
 import cv2
 import pytube
 from ultralytics import YOLO
-from blockchain import write_block
-from deepface import DeepFace
 from datetime import datetime
 
 def main():
     try:    
-        # video_path = "videos/1.mp4"
-        # cap = get_file_capture(video_path)
-        cap = get_camera_capture()
+        video_path = "videos/Elon.mp4"
+        cap = get_file_capture(video_path)
+        # cap = get_camera_capture()
 
         # Проверка на наличие файла модели
         if not os.path.exists("yolov8_test/model_test_main_part2/weights/best.pt"):
             raise FileNotFoundError("Файл модели не найден.")
-        model = YOLO("yolov8_test/model_test_main_part2/weights/best.pt")
+        model = YOLO("yolov8_test/model_test_main_part2/weights/best.pt") 
         
         save_dir_full_photo = "images_full_photo"
         save_dir_only_face = "images_only_face"
@@ -53,7 +51,6 @@ def main():
                 cv2.resizeWindow("Yolov8 big test", 640, 360)
                 cv2.imshow("Yolov8 big test", res_plotted)
 
-                # Сохранение результатов с определенным интервалом
                 if current_time - last_frame_time >= save_interval:
                     for box in boxes:
                         xyxy = box.xyxy
@@ -90,23 +87,9 @@ def main():
                         if cropped_img.size > 0:
                             current_time_spec = datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
                             cv2.imwrite(f"{save_dir_only_face}/op_{count_op}_{class_name}_{float(number_string_conf)}_{conf_hash}_{current_time_spec}.jpg", cropped_img)
-                            if int_number_id==2:
-                                result = DeepFace.analyze(img_path=cropped_img, actions=['age', 'gender', 'emotion', 'race'], enforce_detection=False)
-                                age = result[0]['age']
 
-                                gender_probabilities = result[0]['gender']
-                                genderM = gender_probabilities['Man']
-                                genderW = gender_probabilities['Woman']
-
-                                emotions_dict = result[0]['emotion']
-                                sorted_emotions_dict = sorted(emotions_dict.items(), key=lambda x: x[1], reverse=True)
-
-                                races = result[0]['race']
-                                sorted_races = sorted(races.items(), key=lambda x: x[1], reverse=True)
-
-                            write_block(f"op_{count_op}_{class_name}_{float(number_string_conf)}_{conf_hash}_{current_time_spec}.jpg", 
-                                class_name, age, genderM, genderW, sorted_emotions_dict, sorted_races)
-
+                    cv2.imwrite(f"{save_dir_full_photo}/fp_{count_fp}.jpg", res_plotted)
+                    
                     count_op += 1
                     count_fp += 1
 
@@ -145,7 +128,6 @@ def draw_frame_on_cropped_img(cropped_img: cv2.imread, text: str, ph_conf: float
     h, w, _ = cropped_img.shape
     cv2.rectangle(framed_cropped_img, (0, 0), (w, h), color, thickness)
 
-    # Настройка параметров шрифта
     font = cv2.FONT_HERSHEY_SIMPLEX
     font_color = (0, 0, 255)
     font_thickness = 2
@@ -161,7 +143,6 @@ def draw_frame_on_cropped_img(cropped_img: cv2.imread, text: str, ph_conf: float
 
     cv2.putText(framed_cropped_img, text, (text_x, text_y), font, font_scale, font_color, font_thickness)
 
-    # Добавление дополнительной строки текста
     second_line_text = str(round(ph_conf, 2))
     second_line_text_width = cv2.getTextSize(second_line_text, font, font_scale, font_thickness)[0][0]
     second_line_text_x = (w - second_line_text_width) // 2
@@ -170,14 +151,12 @@ def draw_frame_on_cropped_img(cropped_img: cv2.imread, text: str, ph_conf: float
 
     return framed_cropped_img
 
-"""Возвращает путь к последнему созданному файлу в указанной директории."""
 def get_last_created_file_in_images_full_photo(directory):
     files = os.listdir(directory)
     paths = [os.path.join(directory, file) for file in files]
     paths.sort(key=os.path.getmtime)
     return paths[-1] if paths else None
 
-"""Очищает содержимое указанной директории."""
 def clear_directory(directory: str):
     for file in os.listdir(directory):
         file_path = os.path.join(directory, file)
